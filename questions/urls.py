36e76ac4c6a647014054bc7d7ab5522a738c3b62
ihababbas <ihababbas26@gmail.com>
2023-07-20 11:02:12 +0300

    upload and downlaod

--- questions/urls.py
+++ questions/urls.py
@@ -4,6 +4,6 @@ from .views import upload_csv, download_csv, QuestionsDataDetailView ,QuestionsD
 urlpatterns = [
     path('upload/', upload_csv, name='upload_csv'),
     path('download/', download_csv, name='download_csv'),
-    path('mymodel/<int:pk>/', QuestionsDataDetailView.as_view(), name='mymodel_detail'),
-    path('view/',QuestionsDataListView.as_view(),name='list')
+    path('question/<int:pk>/', QuestionsDataDetailView.as_view(), name='question_detail'),
+    path('questions/',QuestionsDataListView.as_view(),name='questions')
 ]
--- questions/views.py
+++ questions/views.py
@@ -10,15 +10,45 @@ from django.http import HttpResponse
 import io
 
 class QuestionsDataDetailView(RetrieveUpdateDestroyAPIView):
+    """
+      A view for listing and creating QuestionsData objects.
+
+
+    Args:
+    
+        ListCreateAPIView (class): Django Rest Framework's ListCreateAPIView class
+            used for handling both list (GET) and create (POST) operations.
+
+    """
     queryset = QuestionsData.objects.all()
     serializer_class = QuestionsDataSerializer
 
 class QuestionsDataListView(ListCreateAPIView):
+    """
+     A view for listing and creating QuestionsData objects.
+
+    Args:
+        ListCreateAPIView (class): Django Rest Framework's ListCreateAPIView class
+            used for handling both list (GET) and create (POST) operations.
+
+    """
     queryset=QuestionsData.objects.all()
     serializer_class= QuestionsDataSerializer
 
 
 def upload_csv(request):
+    """
+    Handle the CSV file upload and save data to the database.
+
+    Args:
+        equest (HttpRequest): The HTTP request object containing the form data.
+
+
+    Returns:
+        HttpResponse: If the request method is POST and the form is valid, it returns
+        the success.html template with the uploaded data in the context. Otherwise,
+        it returns the upload.html template with the upload form.
+    """
     if request.method == 'POST':
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
@@ -39,25 +69,88 @@ def upload_csv(request):
     return render(request, 'upload.html', {'form': form})
 
 
-
-
-
-
-
-
-
 def download_csv(request):
-    response = HttpResponse(content_type='text/csv; windows-1256 charset=utf-8')
+    """
+        Download CSV file containing QuestionsData.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The HTTP response containing the CSV file.
+
+    """
+    # Create an HTTP response with content type 'text/csv; charset=utf-8'
+    response = HttpResponse(content_type='text/csv; charset=utf-8')
+    # Set the response header to suggest downloading the file as 'data.csv'
     response['Content-Disposition'] = 'attachment; filename="data.csv"'
 
+    # Create a CSV writer to write data into the response
     writer = csv.writer(response)
-    writer.writerow(['type', 'questions','correct','wrong1','wrong2','wrong3'])  # Write header row
+    # Write the header row to the CSV file with column names: 'type', 'questions', 'correct', 'wrong1', 'wrong2', 'wrong3'
+    writer.writerow(['type', 'questions', 'correct', 'wrong1', 'wrong2', 'wrong3'])
 
-    my_models = QuestionsData.objects.all()
-    for my_model in my_models:
-        writer.writerow([my_model.type, my_model.questions,my_model.correct,my_model.wrong1,my_model.wrong2,my_model.wrong3])
+    # Retrieve all objects from the QuestionsData model
+    questions = QuestionsData.objects.all()
+    # Loop through each QuestionsData object and write its data to a row in the CSV file
+    for question in questions:
+        writer.writerow([question.type, question.questions, question.correct, question.wrong1, question.wrong2, question.wrong3])
 
+    # Return the response, which contains the CSV file with data from the QuestionsData model
     return response
 
 
 
+
+
+
+
+
+
+
+# def upload_csv(request):
+#     # Check if the form has been submitted (POST method)
+#     if request.method == 'POST':
+#         # Create an instance of the UploadForm, using the form data from the request
+
+#         form = UploadForm(request.POST, request.FILES)
+#         # Check if the form data is valid
+
+#         if form.is_valid():
+#             # Retrieve the uploaded CSV file from the form
+#             csv_file = request.FILES['csv_file']
+
+#             # Decode the CSV file content from bytes to a string using utf-8 encoding
+#             decoded_file = csv_file.read().decode('utf-8')
+
+#             # Create a StringIO object to read the decoded CSV content line by line
+#             io_string = io.StringIO(decoded_file)
+
+#             # Skip the header row (first row) since it contains column names
+#             next(io_string)
+
+#             # Initialize an empty list to store the data from each row of the CSV file
+#             data = []
+
+#             # Iterate over each row in the CSV file
+#             for row in csv.reader(io_string):
+
+#                 # Extract the 'name' and 'email' values from the current row
+#                 name, email = row
+
+#                 # Create a MyModel object with the extracted data and save it to the database
+#                 MyModel.objects.create(name=name, email=email)
+
+#                 # Append the 'name' and 'email' values to the 'data' list for later use
+#                 data.append({'name': name, 'email': email})
+
+#             # After processing all rows, render the 'success.html' template
+#             # and pass the 'data' list as a context variable for displaying the uploaded data
+#             return render(request, 'success.html', {'data': data})
+
+#     else:
+#         # If the form hasn't been submitted (GET method), create an empty instance of the UploadForm
+#         form = UploadForm()
+
+#     # Render the 'upload.html' template with the UploadForm instance for displaying the upload form
+#     return render(request, 'upload.html', {'form': form})
